0a36f609c35cb044f31eebc8b6b1d8983af876cb
Krishan Solanki <krish@Krishans-MacBook-Pro.local>
2016-03-25 20:35:44 +0000

    project structure updates

--- src/main.c
+++ src/main.c
@@ -1,13 +1,15 @@
 //
 //  main.c
-//  
+//  c-experiments
 //
 //  Created by Krishan Solanki on 25/03/2016.
-//
+//  Copyright (c) 2016 Krishan Solanki. All rights reserved.
 //
 
-#include "main.h"
+#include <stdio.h>
 
-int main(void) {
-    printf("hello world");
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
 }
