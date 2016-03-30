b08799ee14722b0f5aa9fa5387c0d77dafd2335f
Krishan Solanki <krish@Krishans-MacBook-Pro.local>
2016-03-31 00:07:28 +0100

    append node to list list

--- src/linked_list/linked_list.c
+++ src/linked_list/linked_list.c
@@ -8,12 +8,38 @@
 
 #include "linked_list.h"
 
-struct node *createNode( int data) {
+static struct node *createNode(int data) {
     struct node *pTempNode;
     pTempNode = malloc(sizeof(struct node));
-    pTempNode->data = data;
+    
+        pTempNode->data = data;
+
     pTempNode->pLink = NULL;
     
     return pTempNode;
 }
+
+struct node *createHeadNode() {
+    return createNode(0);
+}
+
+void addNodeToList(node *head, int data) {
+    struct node *pTemp = createNode(data);
+    struct node *pCurrent = head;
+    
+    while(pCurrent->pLink) {
+        pCurrent = pCurrent->pLink;
+    }
+    
+    pCurrent->pLink = pTemp;
+}
+
+int count(node *temp)
+{
+    if(temp == NULL)
+        return(0);
+    return(1 + count(temp->pLink));
+}
+
+
 
--- src/tests/linked_list_test.c
+++ src/tests/linked_list_test.c
@@ -11,23 +11,25 @@
 
 static void create_Node_test() {
     
-    struct node *pHead = createNode(0);
-    struct node *a = createNode(5);
-    struct node *b = createNode(10);
-    struct node *c = createNode(15);
-    
+    struct node *pHead = createHeadNode();
+
     assert_null	(pHead->pLink);
     assert_int_equal(pHead->data, 0);
-    assert_null	(a->pLink);
-    assert_int_equal(a->data, 5);
-    assert_null	(b->pLink);
-    assert_int_equal(b->data, 10);
-    assert_null	(c->pLink);
-    assert_int_equal(c->data, 15);
+
 }
 
 static void add_node_test() {
-    //assert_int_equal(count_list(pHead), 4);
+    struct node *pHead = createHeadNode();
+    
+    addNodeToList(pHead, 5);
+    addNodeToList(pHead, 10);
+    addNodeToList(pHead, 15);
+    
+    assert_int_equal(count(pHead), 4);
+    //assert_ptr_equal(pHead.pLink, a);
+    //assert_ptr_equal(a->pLink, b);
+    //assert_ptr_equal(b->pLink, c);
+    //assert_null(c->pLink);
 }
 
 static void delete_node_test() {
